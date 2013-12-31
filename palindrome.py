fe62eb14d3acc15bfa01f279353d9c22e42ac45b
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Compare if a string is palindrome with reversed()

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,7 @@
 # Make a function that receives a word as input and says if it is a palindrome.
 def palindrome(word):
-    return word.lower() == word[::-1].lower()
+    return word.lower() == reversed(word.lower())
+    # return word.lower() == word[::-1].lower() slightly slower with long entries
 
-print(palindrome(input()))    # True or False
+
+print(palindrome(input()))  # True or False
