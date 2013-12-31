03f14d386ce2206b45412cd2d078ee14b6e86374
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Two ways to invert digits from a number in Python.

---
+++ number_inversion.py
@@ -0,0 +1,11 @@
+number = input("Input a number to invert its order:")
+print("Printing using string: " + number[::-1])
+
+inverted = 0
+exponent = len(number)
+number = int(number)
+while number >= 1:
+    inverted += (number % 10) * (10 ** (exponent - 1))
+    exponent -= 1
+    number = number // 10  # the floor division // rounds the result down to the nearest whole number
+print("Printing using elegant math: {}".format(inverted))
