b3b460649325b0bbfc7cc20451c2403e8185e816
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Just some input and print memos in python

--- input_and_print.py
+++ input_and_print.py
@@ -6,7 +6,9 @@ a, b = input('Bora pow\n').split()  # Ler vários números na mesma linha
 a = float(a)  # No Python a entrada por comando input é sempre em string, portanto se precisa converter para outro tipo
 b = float(b)
 
-print("{:.2f}".format(a / b))  # Limita número real à 2 casas decimais
+print(f'{a / b:.2f}')
+# print("{:.2f}".format(a / b)) old form
 # print("O valor da soma é {} e o valor da divisão é {}".format(a + b, a / b))
+
 # <variável> = <valor1> if (True) else <valor2>
 # output_list = [expression for var in input_list if condition]      List Comprehension
