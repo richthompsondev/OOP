91cea6594d2f95339e759fe9306a60dc305196cc
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Just some input and print memos in python

---
+++ input_and_print.py
@@ -0,0 +1,11 @@
+print("Imprimir sem quebra de linha", end='')
+print("o pedido para inserir dois números")
+
+a, b = input('Bora pow\n').split()  # Ler vários números na mesma linha
+
+a = float(a)  # No Python a entrada por comando input é sempre em string, portanto se precisa converter para outro tipo
+b = float(b)
+
+print("{:.2f}".format(a / b))  # Limita número real à 2 casas decimais
+# print("O valor da soma é {} e o valor da divisão é {}".format(a + b, a / b))
+# <variável> = <valor1> if (True) else <valor2>
