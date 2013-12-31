342eb86d35d8afdd9ef9f48e2bf6de073b091ee5
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Delete CheckingAccount.java

--- CheckingAccount.java
+++
@@ -1,8 +0,0 @@
-package com.jony;
-
-public class CheckingAccount /*extends Account*/ {
-    //private final String password;
-
-    //public CheckingAccount(String clientPasword) {
-        //password = clientPasword;
-}
