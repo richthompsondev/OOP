03e58002b18b207f5adf49a556a43359f4b96163
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Bank OO classes
    Constructors and Encapsulation

---
+++ bank/Client.java
@@ -0,0 +1,26 @@
+package bank;
+
+public class Client {
+	private String name;
+	private String cpf; // Social Security or National Insurance Number
+	private String address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
---
+++ bank/Employee.java
@@ -0,0 +1,16 @@
+package bank;
+
+public class Employee {
+	private String name;
+	private String ID;
+	private String office;
+	private double salary;
+	
+	public void giveBonus(){
+		
+	}
+	
+	double calculatesAnnualGain() {
+		return salary*12;
+	}
+}
