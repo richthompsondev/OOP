4ada37ed7639653f951453aa2b8fe5fb65d66940
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Adding total

--- shopping_cart_example.py
+++ shopping_cart_example.py
@@ -4,11 +4,14 @@ from collections import Counter
 def main():
     prices = {"course": 97.99, "book": 54.99, "wallpaper": 4.99}
     cart = Counter(course=1, book=3, wallpaper=2)
+    total = 0.0
 
     for product, units in cart.items():
         subtotal = units * prices[product]
         price = prices[product]
+        total) += subtotal
         print(f"{product:9}: ${price:7.2f} × {units} = ${subtotal:7.2f}")
+        print(total)
 
 
 if __name__ == "__main__":
