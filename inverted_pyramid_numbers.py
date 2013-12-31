a791527d388782f78d2ddfc10ed6727f8ed93dd1
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Pyramid height input.

--- inverted_pyramid_numbers.py
+++ inverted_pyramid_numbers.py
@@ -1,18 +1,17 @@
-import time
-
+# Faça uma função que receba um número n e que imprima uma pirâmide invertida de números:
+# 1, 2, 3, ... n
+# ...
+# 1, 2, 3
+# 1, 2
+# 1
 
 def piramide_invertida(n):
     lista = []
     for i in range(1, n + 1):
         lista.append(i)
-    while len(lista) > 1:
+    while len(lista) >= 1:
         print(str(lista).strip('[]'))
         del(lista[-1])
-    else:
-        print(str(lista).strip('[]'))
 
-
-inicio = time.time()
-piramide_invertida(100) #duracao: 0.006997
-fim = time.time()
-print ('duracao: %f' % (fim - inicio))
+n = int(input())
+piramide_invertida(n)
