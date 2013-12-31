bb8a8d4228bb96e4d5aa3ed0a98dc0260ee849fa
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Delete SpecialClient.java

--- SpecialClient.java
+++
@@ -1,9 +0,0 @@
-package com.jony;
-
-public class SpecialClient extends Client {
-    static Client client  = new Client();
-
-    public void setSpecial() {
-        client.setPriority(3);
-    }
-}
