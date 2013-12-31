522889e27a3f438a501dc776ac87ec13b53a09ef
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Compare if a string is palindrome.

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,5 @@
-# Faça uma função que receba como entrada uma palavra e diga se ela é palíndroma.
-def palindrome(palavra):
-    return palavra.lower() == palavra[::-1].lower()
+# Make a function that receives a word as input and says if it is a palindrome.
+def palindrome(word):
+    return word.lower() == word[::-1].lower()
 
 print(palindrome(input()))    # True or False
