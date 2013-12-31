55e1f0e9d5be3a4fb67b4692a38b04ab79f52f91
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Delete LowIncomeClient.java

--- LowIncomeClient.java
+++
@@ -1,9 +0,0 @@
-package com.jony;
-
-public class LowIncomeClient extends Client{
-    static Client client  = new Client();
-
-    public void setLowIncome() {
-        client.setPriority(1);
-    }
-}
