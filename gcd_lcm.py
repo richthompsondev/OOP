422c14f7f5601a965e9eaf94b2edcb89ea004c63
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Comments

--- gcd_lcm.py
+++ gcd_lcm.py
@@ -41,8 +41,7 @@ def gcd_extended(a, b):
         return b, 0, 1
     gcd, x1, y1 = gcd_extended(b % a, a)
 
-    # Update x and y using results of recursive
-    # call
+    # Update x and y using results of recursive call
     x = y1 - (b // a) * x1
     y = x1
     return gcd, x, y
