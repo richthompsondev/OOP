b047db751e2dfef2ed3bfff7abb9bc79ba932a14
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Just some input and print memos in python
    List Comprehension

--- input_and_print.py
+++ input_and_print.py
@@ -9,3 +9,4 @@ b = float(b)
 print("{:.2f}".format(a / b))  # Limita número real à 2 casas decimais
 # print("O valor da soma é {} e o valor da divisão é {}".format(a + b, a / b))
 # <variável> = <valor1> if (True) else <valor2>
+# output_list = [expression for var in input_list if condition]      List Comprehension
