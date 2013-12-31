f5803119b2733166f8bb71dc03c2676da3c36c7d
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    fix: exception

--- bank/src/com/jony/Connection.java
+++ bank/src/com/jony/Connection.java
@@ -9,12 +9,11 @@ public class Connection implements AutoCloseable{ // Because of try() with resou
     public Connection(){
         System.out.println("Opening connection");
         // For testing object not constructed
-        //throw new IllegalStateException();
     }
 
     public void readData() {
         System.out.println("Receiving data");
-        throw new IllegalStateException();
+        //throw new IllegalStateException();
     }
 
     @Override
