f015d5be5443135867499ef2f7313d3b1df57cd7
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Compare if a string is palindrome
    Using slicing method again

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,7 @@
 # Make a function that receives a word as input and says if it is a palindrome.
-def palindrome(word):
-    return word.lower() == reversed(word.lower())
-    # return word.lower() == word[::-1].lower() slightly slower with long entries
+def palindrome(word):    
+    return word.lower() == word[::-1].lower() 
+    # return word.lower() == reversed(word.lower()) slightly faster with long entries, but reversed() returns an iterator object
 
 
 print(palindrome(input()))  # True or False
