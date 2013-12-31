5787ca104d647f8a5e80136d5893e0dd009d188b
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Rename Classes_Empregado/EmpregadoComissionado.java to classes-empregado/EmpregadoComissionado.java

--- classes-empregado/EmpregadoComissionado.java
+++ classes-empregado/EmpregadoComissionado.java
@@ -1,14 +1,14 @@
 
 
 /**
- * Empregado Comissionado demonstra heranÁa de atributos e mÈtodos de Empregado.
+ * Empregado Comissionado demonstra heran√ßa de atributos e m√©todos de Empregado.
  */
 public class EmpregadoComissionado extends Empregado {
-	private double comissao; // o valor de comiss„o de cada unidade vendida
+	private double comissao; // o valor de comiss√£o de cada unidade vendida
 	private int unidades; // a quantidade de unidades vendidas
 
 	public EmpregadoComissionado(String primeiro_nome, String ultimo_nome, double salario, double comissao) {
-		super(primeiro_nome, ultimo_nome, salario); // chama o mÈtodo construtor original
+		super(primeiro_nome, ultimo_nome, salario); // chama o m√©todo construtor original
 		this.comissao = comissao;
 	}
 
