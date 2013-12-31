df2b6b870d11edb6f1d27d9305fe366c3b738b11
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Applying Override with polymorphism

--- bank/src/com/jony/CheckingAccount.java
+++ bank/src/com/jony/CheckingAccount.java
@@ -1,11 +1,18 @@
 package com.jony;
 
+// Conta corrente
 public class CheckingAccount extends Account {
     public CheckingAccount(String name, int accountNumber, double balance, double limit) {
         super(name, accountNumber, balance, limit);
     }
+
+    @Override
+    synchronized boolean withdrawValue(double value) {
+        double amountToWithdraw = value + 0.20;
+        return super.withdrawValue(amountToWithdraw);
+    }
     //private final String password;
 
-    //public CheckingAccount(String clientPasword) {
-        //password = clientPasword;
+    //public CheckingAccount(String clientPassword) {
+        //password = clientPassword;
 }
