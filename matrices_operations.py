eb4378fbd325762603261fa208ec626707119d3b
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Using NumPy array operations

--- matrices_operations.py
+++ matrices_operations.py
@@ -13,6 +13,14 @@ result = [[X[i][j] + Y[i][j]  for j in range(len(X[0]))] for i in range(len(X))]
 for r in result:
     print(r)
 
+# Using NumPy
+import numpy as np
+
+A = np.array([[2, 4], [5, -6]])
+B = np.array([[9, -3], [3, 6]])
+C = A + B      # element wise addition
+print(C)
+
 # Program to transpose a matrix using list comprehension
 
 X = [[12, 7],
@@ -23,6 +31,10 @@ result = [[X[j][i] for j in range(len(X))] for i in range(len(X[0]))]
 
 for r in result:
     print(r)
+
+# Using NumPy
+A = np.array([[1, 1], [2, 1], [3, -3]])
+print(A.transpose())
 
 # Program to multiply two matrices using list comprehension
 
@@ -41,3 +53,9 @@ result = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*Y)] for X_row 
 
 for r in result:
     print(r)
+
+# Using NumPy
+A = np.array([[3, 6, 7], [5, -3, 0]])
+B = np.array([[1, 1], [2, 1], [3, -3]])
+C = A.dot(B)
+print(C)
